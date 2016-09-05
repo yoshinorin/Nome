d6e72b8b71940058e8ee2fe7eec83f0825fa3018
YoshinoriN <nayo-msoft@outlook.jp>
2016-09-05 22:32:38 +0900

    Create Configuration class and test code.

---
+++ Source/Nome.Configuration/Configuration.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Nome
+{
+    public class ConfigurationManager
+    {
+        public Configuration Configuration { get; private set; }
+
+        public ConfigurationManager(string input)
+        {
+            var json = JObject.Parse(input);
+            this.Configuration = JsonConvert.DeserializeObject<Nome.Configuration>(json["configuration"].ToString());
+        }
+    }
+
+    [JsonObject("configuration")]
+    public class Configuration
+    {
+        [JsonProperty("connection")]
+        public Connection Connection { get; set; }
+    }
+
+    [JsonObject("connection")]
+    public class Connection
+    {
+        [JsonProperty("host")]
+        public string Host { get; set; }
+
+        [JsonProperty("user")]
+        public string User { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+    } 
+}
---
+++ Test/Nome.Configuration.Test/Configuration.Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Nome.Configuration.Test
+{
+    [TestClass]
+    public class ConfigurationTest
+    {
+        private static StreamReader sr = new StreamReader("../../../Source/Nome.Configuration/config/config.json");
+        private string jsonString = sr.ReadToEnd();
+
+        [TestMethod]
+        public void ConstractorTest()
+        {
+            var conf = new Nome.ConfigurationManager(jsonString);
+            Assert.AreEqual("LDAP://127.0.0.1/dc=maxcrc,dc=com", conf.Configuration.Connection.Host);
+        }
+    }
+}
