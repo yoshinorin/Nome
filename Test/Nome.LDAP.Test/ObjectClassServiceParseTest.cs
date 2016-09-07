cc73420975fb7437769bfe38fbda1fab5cbe908d
YoshinoriN <nayo-msoft@outlook.jp>
2016-09-08 00:59:38 +0900

    Add test code of ObjectClassService

---
+++ Test/Nome.LDAP.Test/ObjectClassServiceParseTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nome.LDAP.Test
+{
+    [TestClass]
+    public class ObjectClassServiceParseTest
+    {
+        [TestMethod]
+        public void GetUserTest()
+        {
+            Assert.AreEqual("ユーザー",ObjectClassService.NameByLanguage(ObjectClassTypeEnum.user));
+        }
+
+        [TestMethod]
+        public void GetComputerTest()
+        {
+            Assert.AreEqual("コンピューター", ObjectClassService.NameByLanguage(ObjectClassTypeEnum.computer));
+        }
+    }
+}
