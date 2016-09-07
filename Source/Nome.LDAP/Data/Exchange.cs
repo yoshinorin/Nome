2512bc48fc945af31c58db2f1906b78ce99300ad
YoshinoriN <nayo-msoft@outlook.jp>
2016-09-07 22:00:43 +0900

    Change LDAP project platform, PCL to .NET Framework and add data store classes.

---
+++ Source/Nome.LDAP/Data/Exchange.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// Data store class for Exchange Server.
+/// </summary>
+namespace Nome.LDAP.Data
+{
+    /// <summary>
+    /// Store Exchange Server's data. If Active Directory relate with Exchange.
+    /// </summary>
+    public class Exchange : ActiveDirectory
+    {
+    }
+}
---
+++ Source/Nome.LDAP/Data/LDAP.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// Data store class for LDAP.
+/// </summary>
+namespace Nome.LDAP.Data
+{
+    /// <summary>
+    /// Store LDAP (OpenLDAP)'s data.
+    /// </summary>
+    public class LDAP
+    {
+    }
+}
