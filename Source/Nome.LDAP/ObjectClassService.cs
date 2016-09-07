db044bab6e45e2474a0496811f58f36460facb8a
YoshinoriN <nayo-msoft@outlook.jp>
2016-09-08 00:38:02 +0900

    Add enum of ldap's objectclasses

---
+++ Source/Nome.LDAP/ObjectClassService.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// Provide function of LDAP.
+/// </summary>
+namespace Nome.LDAP
+{
+    /// <summary>
+    /// Provide feature of LDAP Object Class.
+    /// </summary>
+    public static class ObjectClassService
+    {
+        /// <summary>
+        /// Get names of <see cref="ObjectClassTypeEnum"> by language.
+        /// </summary>
+        /// <param name="objectClassEnum"><see cref="ObjectClassTypeEnum"></param>
+        /// <returns>Converted Value by language.</returns>
+        /// TODO : Can change language of return value using by setting file.
+        public static string NameByLanguage(this ObjectClassTypeEnum objectClassEnum)
+        {
+            //Currently japanese only.
+            string[] names = { "ユーザー", "コンピューター" };
+            return names[(int)objectClassEnum];
+        }
+    }
+}
---
+++ Source/Nome.LDAP/ObjectClassTypeEnum.cs
@@ -0,0 +1,38 @@
+﻿/// <summary>
+/// Provide function of LDAP.
+/// </summary>
+namespace Nome.LDAP
+{
+    /// <summary>
+    /// Type of Object class.
+    /// </summary>
+    public enum ObjectClassTypeEnum
+    {
+        user,
+        computer
+    }
+
+    #region "Not implement ObjectClasses"
+
+    /*
+     country
+     locality
+     organization
+     organizationalUnit
+     organizationalPerson
+     organizationalRole
+     groupOfNames
+     residentialPerson
+     applicationProcess
+     applicationEntity
+     dSA
+     device
+     strongAuthenticationUser
+     certificationAuthority
+     groupOfUniqueNames
+     userSecurityInformation
+     certificationAuthority-V2
+    */
+
+    #endregion
+}
