66ff996cce46e5a7d9932b019d4fb95e4d3c42d6
YoshinoriN <nayo-msoft@outlook.jp>
2016-09-21 06:37:35 +0900

    Add test code for Nome.Viewer

---
+++ Test/Nome.Viewer.Test/Services/SearchConditionParseTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nome.Viewer.Services;
+
+namespace Nome.Viewer.Test
+{
+    [TestClass]
+    public class SearchConditionParseTest
+    {
+        [TestMethod]
+        public void GetEqualTest()
+        {
+            Assert.AreEqual("一致", SearchConditionService.NameByLanguage(SearchConditionEnum.equal));
+        }
+
+        [TestMethod]
+        public void GetFowardMatchTest()
+        {
+            Assert.AreEqual("前方一致", SearchConditionService.NameByLanguage(SearchConditionEnum.forwardmatch));
+        }
+
+        [TestMethod]
+        public void GetBackwardMatchTest()
+        {
+            Assert.AreEqual("後方一致", SearchConditionService.NameByLanguage(SearchConditionEnum.backwardmatch));
+        }
+
+        [TestMethod]
+        public void GetContainsTest()
+        {
+            Assert.AreEqual("含む", SearchConditionService.NameByLanguage(SearchConditionEnum.contains));
+        }
+    }
+}
